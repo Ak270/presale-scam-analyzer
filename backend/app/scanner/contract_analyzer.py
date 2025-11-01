dd36a55565547f83f471866fd35cd68bb58a4a1b
Ak270 <amarkelotra@gmail.com>
2025-11-01 21:36:42 +0530

    Initial project skeleton

---
+++ backend/app/scanner/contract_analyzer.py
@@ -0,0 +1,3 @@
+# contract_analyzer placeholder
+def analyze_contract(address, chain):
+    return {"address": address, "chain": chain}
---
+++ frontend/src/pages/token/[token].tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function TokenPage() {
+  return <div>Token page placeholder</div>;
+}
